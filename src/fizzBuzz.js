95e74e7497a5007dba40842e1dd651848d8374af
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-02-04 17:02:37 +0000

    added iteration

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,14 +1,17 @@
 function fizzBuzz(num){
-	if (num % 15 === 0){
-		return "FizzBuzz"
-	}
-	else if (num % 5 === 0){
-		return "Buzz"
-	}
-	else if (num % 3 === 0){
-		return "Fizz"
-	}
-	else {
-		return num
+	for (let i = 1; i <= num; i++){
+
+		if (i % 15 === 0){
+			console.log("FizzBuzz")}
+
+		else if (i % 5 === 0){
+			console.log("Buzz")}
+
+		else if (i % 3 === 0){
+			console.log("Fizz")}
+		
+		else {
+			console.log(i)}
 	};
 };
+
